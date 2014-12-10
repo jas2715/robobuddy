857756a34e2cbc7898c45fae480f8dbd745e0bf8
jas2715 <jas2715@rit.edu>
2014-12-09 19:19:27 -0500

    Menu art

--- example/test/MenuScreen.py
+++ example/test/MenuScreen.py
@@ -10,6 +10,7 @@ class MenuScreen:
 
         # Images
         self.playButton = TextureLoader.load(os.path.join('assets', 'button-start.png'), (154,67))
+        self.background = TextureLoader.load(os.path.join('assets', 'menu.png'), (800,600))
 
         # Two sizes of the same font to use
         self.titleFont =  pygame.font.SysFont('ActionIsShaded', 96)
@@ -22,20 +23,23 @@ class MenuScreen:
         print(self.mouseY)
         
         # Is the user clicking on the Go button?
-        tempX = 400-(154/2)
-        tempY = 300-(67/2)
+        tempX = 65
+        tempY = 250
         if(self.mouseX > tempX) and (self.mouseX < tempX + 154) and (self.mouseY > tempY) and (self.mouseY < tempY + 67):
             self.clicked = True
 
     # Draw all screen elements here!
     def draw(self, screen):
     	if screen != 0:
+            # Background image
+            DrawHelper.drawCoor(screen,self.background,0,0)
+            
              # Text blocks
             title1 = self.titleFont.render("Robobuddy", 1, (10,10,10))
             title1pos = title1.get_rect()
-            title1pos.centerx = 400
-            title1pos.centery = 200
+            title1pos.centerx = 250
+            title1pos.centery = 150
             screen.blit(title1, title1pos)
 
             # Buttons
-            DrawHelper.drawCoor(screen,self.playButton,400-(154/2), 300-(67/2))
+            DrawHelper.drawCoor(screen,self.playButton,65,250)
